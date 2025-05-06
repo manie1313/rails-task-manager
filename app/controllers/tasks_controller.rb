6f17fb9990c384f743f81c2fbb9506acb2ea31d2
manel boulaabi <nuwr24@gmail.com>
2025-05-06 17:46:01 +0100

    models added to our ruby on rails & CRUD

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # raise
+    # one task per id so convention name @task sg and not plr
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # raise
+    @task = Task.new(task_params)
+    @task.save
+    # redirect to show url page where with the new instance of a task
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # redirect to show url page where with the new instance of a task
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # redirect to show url page where with the new instance of a task
+    redirect_to task_path(@task), status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+
+end
